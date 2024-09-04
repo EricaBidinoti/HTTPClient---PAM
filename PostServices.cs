457cf1534792b44d25a799ca86abf1ee589a179c
Erica Bidinoti <162503423+EricaBidinoti@users.noreply.github.com>
2024-09-03 21:01:31 -0300

    Add files via upload

---
+++ PostServices.cs
@@ -0,0 +1,64 @@
+﻿using CommunityToolkit.Mvvm.Collections;
+using HTTPClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace HTTPClient.Services
+{
+    public class PostServices
+    {
+        internal async Task<ObservableCollection<Post>> getPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class Postservices
+        {
+            private HttpClient _httpClient;
+            private Post post;
+            private ObservableCollection<Post> posts;
+            private HttpClient httpClient;
+            private JsonSerializerOptions jsonSerializerOptions;
+
+
+            public Postservices()
+            {
+                httpClient = new HttpClient();
+                jsonSerializerOptions = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    WriteIndented = true,
+                };
+
+            }
+
+            public async Task<ObservableCollection<Post>>getPosts()
+            {
+                Uri uri = new Uri("https://jsonplaceholder.typicode.com/posts");
+                List<Post> itens = new List<Post>();
+
+                try
+                {
+                    HttpResponseMessage response = await httpClient.GetAsync(uri);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string content = await response.Content.ReadAsStringAsync();
+                        itens = JsonSerializer.Deserialize<List<Post>>(content, jsonSerializerOptions);
+                    }
+                }
+                catch(Exception ex)
+                {
+                 
+                }
+                return itens;
+            }
+
+
+        }
+    }
+}
---
+++ PostViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HTTPClient.Models;
+using HTTPClient.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HTTPClient.ViewsModels
+{
+    public partial class PostViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        ObservableCollection<Post> posts;
+
+        private ICommand getPostsCommand { get; }
+
+        public PostViewModel()
+        {
+            getPostsCommand = new Command(getPosts);
+        }
+
+
+        public async void getPosts()
+        {
+            PostServices postServices = new PostServices();
+
+            posts = await postServices.getPosts();
+        }
+
+
+    }
+}
